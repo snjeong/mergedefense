d881543016edf8d695b08ec423ea037c6d4730f2
snjeong <snjeong@r2on.com>
2021-12-03 14:36:30 +0900

    code deploy

---
+++ fruttidino-edge/src/main/java/com/fruttidino/edge/contoller/EdgeController.java
@@ -0,0 +1,24 @@
+package com.fruttidino.edge.contoller;
+
+import com.fruttidino.edge.Entity.Greeting;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class EdgeController {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/")
+    public String healthCheck(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format(template, name);
+    }
+
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
