5e8495aa6fdefa883d102c6183be33205f5f0bb2
snjeong <snjeong@r2on.com>
2022-01-18 17:14:25 +0900

    db connection 변경

---
+++ fruttidino-api/src/main/java/com/fruttidino/api/controller/HealthCheck.java
@@ -0,0 +1,19 @@
+package com.fruttidino.api.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController
+public class HealthCheck {
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @GetMapping("/")
+    public String healthCheck(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return String.format(template, name);
+    }
+
+}
