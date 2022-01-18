405bf16ab581a9bf8a6b2d59e89c18ed76dc4737
snjeong <snjeong@r2on.com>
2022-01-19 01:25:57 +0900

    db connection 변경

--- fruttidino-api/src/main/java/com/fruttidino/api/repository/DinoGenRepository.java
+++ fruttidino-api/src/main/java/com/fruttidino/api/repository/DinoGenRepository.java
@@ -4,11 +4,11 @@ import com.fruttidino.api.entity.game.DinoGen;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
-@RepositoryRestResource
+@Repository
 public interface DinoGenRepository extends JpaRepository<DinoGen, UUID> {
 
     DinoGen findDinoGenByGenId(UUID dinoGenId);
