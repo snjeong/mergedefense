2153eed8e88db8b16a41080405efbae08546b385
snjeong <snjeong@r2on.com>
2022-01-19 02:43:55 +0900

    logging 추가

---
+++ fruttidino-api/src/main/java/com/fruttidino/api/filter/Interceptor.java
@@ -0,0 +1,31 @@
+package com.fruttidino.api.filter;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.lang.Nullable;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Log4j2
+@Component
+public class Interceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        log.info("url : {}", request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) throws Exception {
+        log.info("response status: {}", response.getStatus());
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object object, @Nullable Exception arg3) throws Exception {
+        log.info("url : {}", request.getRequestURI());
+    }
+}
